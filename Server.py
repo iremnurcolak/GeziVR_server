164e2dba8fc0b66e78d3f579c16f31fdf0fc18ac
İremnur Çolak <iremnurcolak34@gmail.com>
2023-03-25 00:58:28 +0300

    Get request for all artists added

--- Server.py
+++ Server.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+allArtistURL = "http://www.wikiart.org/en/App/Artist/AlphabetJson?v=new&inPublicDomain={true/false}"
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    r = requests.get(url = allArtistURL)
+    data = r.json()
+    return data
