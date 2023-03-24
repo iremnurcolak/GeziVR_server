1f0196d20bb7cae0e7ce15f7a71724b8e819867e
İremnur Çolak <iremnurcolak34@gmail.com>
2023-03-25 00:35:40 +0300

    deployment changes

--- Server.py
+++ Server.py
@@ -1,19 +1,8 @@
-from flask import Flask, request
-from flask_cors import CORS
-from flask_restful import Resource, Api
-import requests
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-api = Api(app)
 
 
-class Users(Resource):
-  @app.route('/home', methods = ['GET', 'POST'])
-  def json_example():
-    return "Hello World"
-  pass
-
-
-if __name__ == '__main__':
-  app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
